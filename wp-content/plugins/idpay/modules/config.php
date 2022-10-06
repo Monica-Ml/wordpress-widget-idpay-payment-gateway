cd2127886fa62d3d64d972d7de73de64cecc98b9
hamidarn <crabcraf@gmail.com>
2022-10-05 22:01:55 -0700

    change configs

--- wp-content/plugins/idpay/modules/config.php
+++ wp-content/plugins/idpay/modules/config.php
@@ -9,5 +9,5 @@ ini_set('display_errors','off');// todo off
 //===================================== IDPay settings ===============================================
 define('IDPay_limit_payment_min',10000);
 define('IDPay_limit_payment_max',500000000);
-define('IDPay_API_KEY','0c183875-aac0-480d-ae0d-ad085b5fa383');
-define('IDPay_unique_link','IDPay.ir/mydigifantasy');
+define('IDPay_API_KEY',''); // Your IDPay API KEY
+define('IDPay_unique_link','IDPay.ir/mydigi');
